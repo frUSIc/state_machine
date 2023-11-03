31d477330b1c9a7ef5a36e8a18dcc2a7409ed2da
frUSIc <47726523+frUSIc@users.noreply.github.com>
2023-11-04 00:36:47 +1100

    Initial setup

---
+++ dist/index.js
@@ -0,0 +1,21 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.main = void 0;
+function main() {
+    return __awaiter(this, void 0, void 0, function* () {
+        const thing = 'asd';
+        return thing;
+    });
+}
+exports.main = main;
+main();
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW5kZXguanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyIuLi9zcmMvaW5kZXgudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6Ijs7Ozs7Ozs7Ozs7O0FBQUEsU0FBc0IsSUFBSTs7UUFDeEIsTUFBTSxLQUFLLEdBQUcsS0FBSyxDQUFDO1FBQ3BCLE9BQU8sS0FBSyxDQUFDO0lBQ2YsQ0FBQztDQUFBO0FBSEQsb0JBR0M7QUFFRCxJQUFJLEVBQUUsQ0FBQyJ9
---
+++ tests/index.test.ts
@@ -0,0 +1,5 @@
+import { main } from '../src/index';
+
+test('Stub', async () => {
+  await expect(main()).resolves.toBe('asd');
+});
